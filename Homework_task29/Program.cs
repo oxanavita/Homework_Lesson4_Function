0f6bc363a3881039f24f90729796f35f8472c950
Oksana <oxana.vita91@mail.ru>
2023-06-04 09:46:08 +0300

    Задача 29

---
+++ Homework_task29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[8];
+
+        // Ввод элементов массива
+        Console.WriteLine("Введите 8 элементов массива:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Вывод массива на экран
+        Console.WriteLine("Массив:");
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+}
